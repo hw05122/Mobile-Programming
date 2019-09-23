4b8d2c873e47e51c0f1d11924f9e99347ca7758e
hw05122 <hw05122@naver.com>
2019-09-23 12:30:38 +0900

    hw2

--- hw2/src/main/java/com/example/hw2/MainActivity.java
+++ hw2/src/main/java/com/example/hw2/MainActivity.java
@@ -3,15 +3,49 @@ package com.example.hw2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn1, btn2, btn3;
+    TextView textFrame1, textFrame2;
+    TableLayout tableFrame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn2 = (Button)findViewById(R.id.btn2);
+        btn3 = (Button)findViewById(R.id.btn3);
+        textFrame1 = (TextView)findViewById(R.id.textFrame1);
+        textFrame2 = (TextView)findViewById(R.id.textFrame2);
+        tableFrame = (TableLayout)findViewById(R.id.tableFrame);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        if(view == btn1){
+            textFrame1.setVisibility(View.VISIBLE);
+            textFrame2.setVisibility(View.INVISIBLE);
+            tableFrame.setVisibility(View.INVISIBLE);
+        }
+        else if(view == btn2){
+            textFrame1.setVisibility(View.INVISIBLE);
+            textFrame2.setVisibility(View.VISIBLE);
+            tableFrame.setVisibility(View.INVISIBLE);
+        }
+        else if(view == btn3){
+            textFrame1.setVisibility(View.INVISIBLE);
+            textFrame2.setVisibility(View.INVISIBLE);
+            tableFrame.setVisibility(View.VISIBLE);
+        }
     }
 }
