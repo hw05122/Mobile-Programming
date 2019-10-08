af86630e6fe90eee9dcec0a589bf23c8114e5b60
hw05122 <hw05122@naver.com>
2019-10-08 19:07:45 +0900

    1008

--- 1008-2_context_menu/src/main/java/com/example/a1008_2_context_menu/MainActivity.java
+++ 1008-2_context_menu/src/main/java/com/example/a1008_2_context_menu/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.a1008_2_context_menu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -30,5 +34,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
         super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+
+        if(v == btn1){
+            menu.setHeaderTitle("배경색 변경");
+            menuInflater.inflate(R.menu.menu1, menu);
+        }
+
+        if(v == btn2){
+            menu.setHeaderTitle("버튼 변경");
+            menuInflater.inflate(R.menu.menu2, menu);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemRed:
+                ll.setBackgroundColor(Color.RED);
+                break;
+            case R.id.itemGreen:
+                ll.setBackgroundColor(Color.GREEN);
+                break;
+            case R.id.itemBlue:
+                ll.setBackgroundColor(Color.BLUE);
+                break;
+            case R.id.itemRotate:
+                btn2.setRotation(45);
+                break;
+            case R.id.itemDouble:
+                btn2.setScaleX(2);
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 }
