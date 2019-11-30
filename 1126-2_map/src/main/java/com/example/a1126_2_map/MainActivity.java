a16f93affcde4b22a8bf498b084801abbdc9bfdc
PARKJIYOUNG <hw05122@naver.com>
2019-12-01 03:56:40 +0900

    1126

--- 1126-2_map/src/main/java/com/example/a1126_2_map/MainActivity.java
+++ 1126-2_map/src/main/java/com/example/a1126_2_map/MainActivity.java
@@ -1,20 +1,23 @@
 package com.example.a1126_2_map;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SubMenu;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap gMap;
@@ -25,25 +28,51 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("구글 지도 활용");
-        mapFrag = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
+        setTitle("Google 지도 활용 (수정)");
+
+        mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        gMap = map;
+        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256, 126.897240), 15));
+        gMap.getUiSettings().setZoomControlsEnabled(true);
+
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng point) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(point);
+
+                gMap.addMarker(markerOptions);
+//                videoMark = new GroundOverlayOptions().image(
+//                        BitmapDescriptorFactory
+//                                .fromResource(R.drawable.ic_launcher_foreground))
+//                        .position(point, 100f, 100f);
+//                Toast.makeText(getApplicationContext(), String.valueOf(point), Toast.LENGTH_SHORT).show();
+//                gMap.addGroundOverlay(videoMark);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-
-        menu.add(0,1,0,"위성지도");
-        menu.add(0,2,0,"일반지도");
-        menu.add(0,3,0,"월드컵경기장 바로가기");
-
+        menu.add(0, 1, 0, "위성 지도");
+        menu.add(0, 2, 0, "일반 지도");
+        SubMenu sMenu = menu.addSubMenu("유명장소 바로가기 >>");
+        sMenu.add(0, 3, 0, "정동진");
+        sMenu.add(0, 4, 0, "해운대");
+        sMenu.add(0, 5, 0, "땅끝마을");
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case 1:
                 gMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
                 return true;
@@ -51,29 +80,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
                 return true;
             case 3:
-                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256,126.897240),15));
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        37.689254, 129.033051), 15));
+                return true;
+            case 4:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        35.159003, 129.161882), 15));
+                return true;
+            case 5:
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+                        34.301472, 126.524048), 15));
                 return true;
         }
-
         return false;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        gMap = googleMap;
-        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256,126.897240),15));
-        gMap.getUiSettings().setZoomControlsEnabled(true);
-        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                videoMark = new GroundOverlayOptions().image(BitmapDescriptorFactory.fromResource(R.drawable.common_google_signin_btn_icon_light)).position(latLng,100f,100f);
-                gMap.addGroundOverlay(videoMark);
-            }
-        });
-
-        //위성지도타입
-//        gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-//        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.568256,126.897240),15));
-    }
 }
